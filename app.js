3ad06af07162273928e2be41fc1ec4b5bdac6641
ahonson ahonson <ahonson@gmail.com>
2021-01-05 10:59:34 +0000

    First attempt at forex-socket

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const server = app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+// const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// MongoDB
+// const mongo = require("mongodb").MongoClient;
+// const dsn =  process.env.DBWEBB_DSN || "mongodb://localhost:27017/chat";
+
+io.on('connection', async function (socket) {
+    console.info("User connected");
+    try {
+        let res = await findInCollection(dsn, "posts", {}, {}, 0);
+        io.emit('earlier chat', res)
+    } catch (err) {
+        console.log(err);
+    }
+
+    socket.on('chat message', async function (message) {
+        io.emit('chat message', message);
+        await saveToCollection(dsn, "posts", message);
+        console.log(message);
+    });
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hallo Welt</h1>');
+});
